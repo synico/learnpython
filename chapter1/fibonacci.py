dd69e70577fdd4408ef59c9c98b01ff885d437ae
liuxuewei <liuxuewei@cmsr.chinamobile.com>
2024-09-20 10:36:31 +0800

    add examples about match
    Signed-off-by: liuxuewei <liuxuewei@cmsr.chinamobile.com>

--- chapter1/fibonacci.py
+++ chapter1/fibonacci.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3.8
 
 from sys import argv, path
-
+import json
 
 def fib(n):
     a, b = 0, 1
@@ -14,9 +14,13 @@ def fib(n):
 fib(100)
 
 print('==========python from import===========')
-print('path:', path)
+# print('path:', path)
+for i in path:
+    print(i.ljust(80) + "|")
 
 print('==============start==============')
 for i in argv:
     print(i)
 print('==============end==============')
+
+print(__name__)
--- chapter1/loopStat.py
+++ chapter1/loopStat.py
@@ -12,9 +12,9 @@ def whileLoop():
 
 def forLoop():
     for i in range(5):
-        print("start to loop in range")
+        print("start to loop in range", i)
     else:
-        print("end of for loop")
+        print(i, "end of for loop")
 
     print("outer of for loop")
 
@@ -23,7 +23,28 @@ def forRange():
     for i in range(0, 10, 3):
         print("i in range: ", i)
 
+def forContinue():
+    for i in range(2, 10):
+        if i % 2 == 0:
+            print("i is even number: ", i)
+            continue
+        print("i is odd number:  ", i)
+
+def forSwitch(point):
+    match point:
+        case "1":
+            print("point is 1")
+        case "2":
+            print("point is 2")
+        case "3":
+            print("point is 3")
+        case _:
+            print("point is not in case")
+
+
 
 # whileLoop()
 # forLoop()
-forRange()
+# forRange()
+# forContinue()
+forSwitch("1")
