325396538abf53be4153f037aa521f917f517349
liuxuewei <liuxuewei@cmsr.chinamobile.com>
2024-11-22 17:06:41 +0800

    create QMainWindow by pyqt5
    Signed-off-by: liuxuewei <liuxuewei@cmsr.chinamobile.com>

---
+++ vlc/pyqtMain.py
@@ -0,0 +1,22 @@
+import sys
+import vlc
+from PyQt5.QtWidgets import QApplication, QMainWindow
+import vwyl
+
+class VWYLMainWidget(QMainWindow, vwyl.Ui_MainWindow):
+    def __init__(self):
+        super(VWYLMainWidget, self).__init__()
+        self.setupUi(self)
+
+        instance = vlc.Instance()
+        media_player = instance.media_player_new()
+        media_player.set_hwnd(self.video_frame.winId())
+        media = instance.media_new("rtsp://192.168.0.51:8554/stream")
+        media_player.set_media(media)
+        media_player.play()
+        
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    main_widget = VWYLMainWidget()
+    main_widget.show()
+    sys.exit(app.exec_())
--- vlc/videoplayer.py
+++ vlc/videoplayer.py
@@ -1,35 +1,55 @@
 import sys
 import vlc
-from PyQt5.QtWidgets import QApplication, QMainWindow, QFrame
-from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QWidget, QFrame, QHBoxLayout, QPushButton, QVBoxLayout
 
-class VideoPlayer(QMainWindow):
+class VideoPlayer(QWidget):
     def __init__(self):
         super().__init__()
         self.initUI()
 
     def initUI(self):
-        self.setGeometry(200, 200, 800, 600)
+        self.setGeometry(200, 200, 1000, 600)
         self.setWindowTitle('Video Player')
 
-        self.videoFrame = QFrame(self)
-        self.videoFrame.setGeometry(10, 10, 800, 600)
-        self.videoFrame.resize(800, 600)
-        self.videoFrame.move(0, 0)
-        self.videoFrame.setStyleSheet("background-color: black;")
+        v_layout1 = QVBoxLayout()
+        videoFrame = QFrame()
+        videoFrame.setGeometry(0, 0, 800, 600)
+        # videoFrame.resize(800, 600)
+        # videoFrame.move(0, 0)
+        instance = vlc.Instance()
+        media_player = instance.media_player_new()
+        media_player.set_hwnd(videoFrame.winId())
+        media = instance.media_new("rtsp://192.168.0.51:8554/stream")
+        # media = instance.media_new("rtsp://admin:Sygy%402024@192.168.0.64:554")
+        media_player.set_media(media)
+        media_player.play()
+        v_layout1.addWidget(videoFrame)
 
-        self.instance = vlc.Instance()
-        self.media_player = self.instance.media_player_new()
-        self.media_player.set_hwnd(self.videoFrame.winId())
+        v_layout2 = QVBoxLayout()
+        dataFrame = QFrame()
+        dataFrame.resize(200, 400)
+        # dataFrame.move(800, 0)
+        dataFrame.setStyleSheet("background-color: white;")
+        self.btn = QPushButton("Start")
+        self.btn.clicked.connect(self.change_text)
+        self.btn.setStyleSheet("background-color: gray;")
+        v_layout2.addWidget(dataFrame)
+        v_layout2.addWidget(self.btn)
 
-        # media = self.instance.media_new("rtsp://192.168.0.125:8554/stream")
-        media = self.instance.media_new("rtsp://admin:Sygy%402024@192.168.0.64:554")
-        self.media_player.set_media(media)
-        self.media_player.play()
+        hlayout = QHBoxLayout()
+        hlayout.addLayout(v_layout1)
+        hlayout.addLayout(v_layout2)
+        self.setLayout(hlayout)
+        
+    def change_text(self):
+        if self.btn.text() == "Start":
+            self.btn.setText("Stop")
+        else:
+            self.btn.setText("Start")
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     player = VideoPlayer()
     player.show()
-    sys.exit(app.exec_())
+    sys.exit(app.exec())
 
